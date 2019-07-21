a569f53f732ca72adbd585b57c1d8a44b1c9a2c6
huiLight <huilight@outlook.com>
2019-07-21 20:01:06 +0800

    '将结果写入网页'

--- spider-l/zhihu.py
+++ spider-l/zhihu.py
@@ -23,6 +23,7 @@ def get_question(key_word):
 
     # https验证错误，简单的处理办法是在get方法中加入verify参数，并设为false。
     content = requests.get(seed_url, headers=headers, proxies=proxies, verify=False)
+    content.encoding='utf-8'
 
     # content.text返回为字符串，需要先转化为json才能用jsonpath
     content = json.loads(content.text)
@@ -40,11 +41,28 @@ def get_question(key_word):
         # 加载并格式化下一页内容
         seed_url = jsonpath(content, '$.paging.next')[0]
         content = requests.get(seed_url, headers=headers, proxies=proxies, verify=False)
+        content.encoding='utf-8'
         content = json.loads(content.text)
 
 
     return results
-    
+
+def results2html(key_word, results):
+    """
+    报错： 'gbk'codec can't encode character ...
+    在windows下面，新文件的默认编码是gbk，
+    这样的话，python解释器会用gbk编码去解析我们的网络数据流txt，
+    然而txt此时已经是decode过的unicode编码，这样的话就会导致解析不了，
+    出现问题。 解决的办法就是，改变目标文件的编码：
+    """
+    with open(key_word+'.html', 'w', encoding='utf-8') as f:
+        f.write('<!doctype html> <html> <head> <meta charset="utf-8"></head> <body>')
+        f.write('<p>{}共{:3d}条数据.{}</p>'.format('-'*10, len(results), '-'*10))
+        f.write('<ul>')
+        for k, v in results.items():
+            print(k)
+            f.write('<li><a href="{}" target="_blank">{}</a></li>'.format(v, k))
+        f.write('</ul></body></html>')
 
 if __name__ == '__main__':
 
@@ -52,7 +70,9 @@ if __name__ == '__main__':
     requests.packages.urllib3.disable_warnings()
     key_word = '幽默'
     results = get_question(key_word)
-    for k, v in results.items():
-        print(k, v)
-    print('-'*20)
-    print('Total %d question pages.'%len(results))
+    results2html(key_word, results)
+    # for k, v in results.items():
+    #     print(k, v)
+    # print('-'*20)
+    # print('Total %d question pages.'%len(results))
+    print('--------complish--------')
