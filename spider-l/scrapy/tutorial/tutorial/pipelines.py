f22c3736e540da69c1efe4209bb7be3f984ed38a
huiLight <huilight@outlook.com>
2019-08-05 22:02:20 +0800

    '打开详情页爬去作者，链接，点赞数，内容等信息'

--- spider-l/scrapy/tutorial/tutorial/pipelines.py
+++ spider-l/scrapy/tutorial/tutorial/pipelines.py
@@ -5,7 +5,39 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 
+# # export a list
+# from scrapy.exporters import JsonItemExporter
 
-class TutorialPipeline(object):
+# class QsbkPipeline(object):
+
+#     def open_spider(self, spider):
+#         self.fp = open("qsbk.json", "wb")# , encoding="utf-8")
+#         self.exporter = JsonItemExporter(self.fp, ensure_ascii=False)
+#                                         # encoding='utf-8')
+        # self.exporter.start_exporting()
+
+#     def process_item(self, item, spider):
+#         self.exporter.export_item(item)
+#         return item
+
+#     def close_spider(self, spider):
+#         self.exporter.finish_exporting()
+#         self.fp.close()
+
+
+from scrapy.exporters import JsonLinesItemExporter
+
+class QsbkPipeline(object):
+
+    def open_spider(self, spider):
+        self.fp = open("qsbk.jl", "wb")# , encoding="utf-8")
+        self.exporter = JsonLinesItemExporter(self.fp, ensure_ascii=False,
+                                        encoding='utf-8')
+
     def process_item(self, item, spider):
+        self.exporter.export_item(item)
         return item
+
+    def close_spider(self, spider):
+        self.exporter.finish_exporting()
+        self.fp.close()
--- spider-l/scrapy/tutorial/tutorial/spiders/qsbk_spider.py
+++ spider-l/scrapy/tutorial/tutorial/spiders/qsbk_spider.py
@@ -4,16 +4,38 @@ import scrapy
 class QsbkSpider(scrapy.Spider):
     name = "qsbk"
     # 注意，在这里是start_urls，而不是urls
+    # 需要在设置文件中修改请求头，否则服务器不响应
     start_urls = ['https://www.qiushibaike.com/text/',]
 
 
     def parse(self, response):
-        print('Hello, world')
         content_blocks = response.xpath("//div[@id='content-left']/div")
 
-        for content_block in content_blocks:
-            author = content_block.xpath(".//h2/text()").get().strip()
-            content = content_block.xpath(".//div[@class='content']//text()").getall()
-            content = "".join(content).strip()
+        # for content_block in content_blocks:
+        #     author = content_block.xpath(".//h2/text()").get().strip()
+        #     content = content_block.xpath(".//div[@class='content']//text()").getall()
+        #     content = "".join(content).strip()
 
-            yield {"author":author, "content": content}
+        #     yield {"author":author, "content": content}
+
+        for content_block in content_blocks:
+
+            href = content_block.xpath(".//a[@class='contentHerf']/@href").get()
+            yield response.follow(href, self.content_parse)
+
+
+    def content_parse(self, response):
+
+        author = response.xpath("//span[@class='side-user-name']/text()").get()
+        like = response.xpath("//i[@class='number']/text()").get()
+
+        # 必须是getall()， 否则只能显示第一行
+        content = response.xpath("//div[@class='content']/text()").getall()
+        content = ''.join(content)
+
+        yield {
+                "author" : author,
+                "like" : like,
+                "url" : response.url,
+                "content" : content,
+                }
