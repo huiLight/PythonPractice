72fd61b348939d45309b15a0acf2397656653884
huiLight <huilight@outlook.com>
2019-08-07 22:42:54 +0800

    在重构之前，添加为Driver类添加get方法

--- spider-l/touchClouds/tcTestDriver.py
+++ spider-l/touchClouds/tcTestDriver.py
@@ -7,8 +7,10 @@ class Element(object):
     def click(self):
         print("click")
 
+
     def clear(self):
         print("clear")
+
 
     def send_keys(self, words):
         print(f"send_keys:{words}")
@@ -20,10 +22,19 @@ class TcTestDriver(object):
         pass
 
 
+    def get(self, url):
+        print(url)
+
+
+    def set_page_load_timeout(self, seconds):
+        pass
+
+
     def find_element_by_xpath(self, raise_exception):
         """
         若参数为
         """
+        print(raise_exception)
         if raise_exception:
             raise Exception('Xpath error!')
         else:
